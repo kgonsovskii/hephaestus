b03c2c865a24dc5cfd7e0efb0da5d3f9f685619f
Your Name <you@example.com>
2026-04-08 17:53:04 +0300

    123

--- src/RemoteEnabler/publish.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-dotnet publish "%~dp0RemoteEnabler.csproj" -c Release -r win-x64 --self-contained true -o "..\..\rdp"
--- src/RemoteEnabler/runme.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-dotnet run --project "%~dp0RemoteEnabler.csproj" -- 78.140.243.76 Administrator W0HmJkdBFyArO061
